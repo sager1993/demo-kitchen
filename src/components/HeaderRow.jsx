fcd16641c94144d750309a69aebed9e2be1dcd8d
Sager Alarifi <sagr.1993@gmail.com>
2019-11-07 11:28:10 +0300

    style title

--- src/components/HeaderRow.jsx
+++ src/components/HeaderRow.jsx
@@ -3,8 +3,8 @@ import Grid from "@material-ui/core/Grid";
 
 const HeaderRow = props => {
   return (
-    <Grid xs={12} item style={{ backgroundColor: "grey", color: "white" }}>
-      <h1 style={{ padding: "10px" }}>The Recipe Guru</h1>
+    <Grid xs={12} item style={{ backgroundColor: "#CC0000", color: "white" }}>
+      <h1 style={{ padding: "10px", textAlign: "center" }}>The Recipe Guru</h1>
       <div style={{ padding: "10px" }}>{props.isLoading ? `LOADING!` : ``}</div>
     </Grid>
   );
